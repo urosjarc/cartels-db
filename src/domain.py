842084eab8967b3ccbe3fa7b30a0e8a420368ebf
urosjarc <jar.fmf@gmail.com>
2020-06-16 14:46:27 +0200

    Init commit

---
+++ src/domain.py
@@ -0,0 +1,11 @@
+
+class Data:
+    def __init__(self):
+        Firm
+        Case
+        Undertaking
+        Holding
+        GC_Case_number
+        Ticker_firm
+        Ticker_undertaking
+        Holding_Ticker_parent
