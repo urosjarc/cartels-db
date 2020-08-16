2e66001ac8389737b0c44d1dfad3573c93c1b648
urosjarc <jar.fmf@gmail.com>
2020-08-16 17:21:32 +0200

    Adding more variables, adding countryinfo.py elements.

--- src/analysis.py
+++ src/analysis.py
@@ -8,6 +8,7 @@ this.matcher = None
 
 db.init_db()
 
+
 def EC_duration():
     '''
     EC_duration (datum izdaje odločbe minus (najstarejši datum enega od stolpcev (Readoption_amendment Ex offo  Notification Complaint Leniency Statement of objections Dawn raid))
@@ -25,12 +26,14 @@ def EC_duration():
             case['Dawn_raid']
         )
 
+
 def EC_decision_year():
     '''
     EC_decision_year (vstavi se samo leto izdaje odločbe)
     '''
     for case in list(db.matcher.match('Case')):
         print(case['EC_Date_of_decision'].split('/')[-1])
+
 
 def EC_dec_may_2004():
     '''
@@ -43,10 +46,12 @@ def EC_dec_may_2004():
         dummy = (weightDate - date).days > 0
         print(dummy)
 
+
 def EC_dec_EN():
-    #(dummy, 01, če je 1, če ni 0)
+    # (dummy, 01, če je 1, če ni 0)
     for case in list(db.matcher.match('Case')):
-        print(len(case['Case_File']) > 0, str(case['Case_File']).isspace(),'\t',case['Case_File'])
+        print(len(case['Case_File']) > 0, str(case['Case_File']).isspace(), '\t', case['Case_File'])
+
 
 def N_firms_within_EC_case():
     '''
@@ -54,8 +59,9 @@ def N_firms_within_EC_case():
     '''
     for case in list(db.matcher.match('Case')):
         num = db.graph.run('MATCH (Firm)-[r]->(c:Case) where c.Case=$case RETURN count(r)',
-                         case=case['Case']).data()[0]['count(r)']
-        print(num,'\t' ,case['Case'])
+                           case=case['Case']).data()[0]['count(r)']
+        print(num, '\t', case['Case'])
+
 
 def N_firms_within_under():
     '''
@@ -64,7 +70,8 @@ def N_firms_within_under():
     for undertaking in list(db.matcher.match('Undertaking')):
         num = db.graph.run('MATCH (f:Firm)-[r]->(u:Undertaking) where u.Undertaking=$undertaking RETURN count(r)',
                            undertaking=undertaking['Undertaking']).data()[0]['count(r)']
-        print(num, '\t' ,undertaking['Undertaking'])
+        print(num, '\t', undertaking['Undertaking'])
+
 
 def Multiple_firm_under():
     '''
@@ -73,7 +80,8 @@ def Multiple_firm_under():
     for undertaking in list(db.matcher.match('Undertaking')):
         num = db.graph.run('MATCH (f:Firm)-[r]->(u:Undertaking) where u.Undertaking=$undertaking RETURN count(r)',
                            undertaking=undertaking['Undertaking']).data()[0]['count(r)']
-        print(1 if num>1 else 0, '\t' ,undertaking['Undertaking'])
+        print(1 if num > 1 else 0, '\t', undertaking['Undertaking'])
+
 
 def Repeat_firm_N_EC_cases():
     '''
@@ -82,19 +90,22 @@ def Repeat_firm_N_EC_cases():
     for firm in list(db.matcher.match('Firm')):
         num = db.graph.run('MATCH (f:Firm)-[r]->(c:Case) where f.Firm=$firm RETURN count(r)',
                            firm=firm['Firm']).data()[0]['count(r)']
-        print(num, '\t' ,firm['Firm'])
+        print(num, '\t', firm['Firm'])
+
 
 def Recidivist_firm_D():
     '''Recidivist_firm_D (dummy 01, če se pojavi samo enkrat v Case-ih, potem, če se pojavi vsaj 2-krat, potem 1)'''
     for firm in list(db.matcher.match('Firm')):
         num = db.graph.run('MATCH (f:Firm)-[r]->(c:Case) where f.Firm=$firm RETURN count(r)',
                            firm=firm['Firm']).data()[0]['count(r)']
-        print(1 if num >= 2 else 0, '\t' ,firm['Firm'])
+        print(1 if num >= 2 else 0, '\t', firm['Firm'])
+
 
 def Recidivist_firm_2nd_time_D():
     '''
     Todo: Recidivist_firm_2nd_time_D (dummy 01, ko se firma, ki je recidivist datumsko glede na EC_Date_of_decision prvič pojavi v bazi je tudi 0 (in ne 1 kot pri prejšnjem dummy-ju), ko se pa pojavi časovno gledano drugič, tretjič itd. pa je 1)
     '''
+
 
 def N_Firm_Inc_states_within_EC_case():
     '''
@@ -102,34 +113,203 @@ def N_Firm_Inc_states_within_EC_case():
     '''
     for case in list(db.matcher.match('Case')):
         firms_r = db.graph.run('MATCH (f:Firm)-[r]->(c:Case) where c.Case=$case RETURN f',
-                           case=case['Case']).data()
+                               case=case['Case']).data()
 
         states = set()
         for firm_r in firms_r:
             states.add(firm_r['f']['Incorporation_state'])
-        print(len(states),case['Case'], sep='\t->\t')
+        print(len(states), case['Case'], sep='\t->\t')
+
 
 def European_firm():
-    '''Todo: make this happend...'''
-    for firm in list(db.matcher.match('Firm')):
-        try:
-            country = pycountry.countries.search_fuzzy(firm['Incorporation_state'])
-        except:
-            country = []
-        c_r = None
-        for c in country:
-            if c.name == firm['Incorporation_state']:
-                c_r = c
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        cinfo = utils.getCountryInfo(country)
+        print(cinfo.get('continent'), country)
+
+
+def Extra_Europe_firm():
+    extra_eu = [
+        'Australia', 'Bermuda', 'British Virgin Islands', 'Canada',
+        'Cayman Islands', 'Hong Kong', 'India', 'Israel', 'Malaysia',
+        'New Zealand', 'Republic of South Africa', 'Singapore', 'USA', 'Brasil',
+        'Chile', 'Kuwait', 'Mexico', 'Tunisia', 'China', 'Japan', 'South Korea', 'Taiwan',
+    ]
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country in extra_eu)
+
+
+def EU_all_time_firm():
+    '''
+    Todo: ????
+    EU_all_time_firm(dummy 01, če je bila kadarkoli v EU, 1, če ne 0) '''
+
+
+def Old_EU_firm():
+    '''
+    Old_EU_firm(če gre za ustavno članico EU iz l. 1952, potem 1, drugače 0) '''
+    firms = list(db.matcher.match('Firm'))
+    EU_founders = ['Belgium', 'France', 'Germany', 'Italy', 'Luxembourg', 'Netherland']
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country in EU_founders)
+
+
+def USA_firm():
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country == 'USA')
+
+
+def Japan_firm():
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country == 'Japan')
+
+
+def Common_Law_Firm():
+    countries_common_law = [
+        'Australia', 'Bermuda', 'British Virgin Islands', 'Canada',
+        'Cayman Islands', 'Channel Islands', 'Hong Kong', 'India',
+        'Ireland', 'Israel', 'Malaysia', 'New Zealand', 'Republic of South Africa',
+        'Singapore', 'UK', 'USA']
+
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country in countries_common_law)
+
+
+def English_Law_Firm():
+    '''
+    English_Law_Firm (dummy 01, če je English 1, če ni 0)
+    '''
+
+    english_law = [
+        'Australia', 'Bermuda', 'British Virgin Islands', 'Canada', 'Cayman Islands', 'Channel Islands',
+        'Hong Kong', 'India', 'Ireland', 'Israel', 'Malaysia', 'New Zealand', 'Republic of South Africa', 'Singapore',
+        'UK', 'USA']
+
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country in english_law)
+
+
+def French_Law_Firm():
+    french_law = [
+        'Belgium', 'Brasil', 'Chile', 'France',
+        'Greece', 'Italy', 'Kuwait', 'Lithuania',
+        'Luxembourg', 'Mexico', 'Netherlands', 'Portugal',
+        'Romania', 'Spain', 'Tunisia'
+    ]
+
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country in french_law)
+
+
+def German_Law():
+    german_law = [
+        'Austria', 'China', 'Croatia', 'Czech Republic', 'Estonia', 'Germany',
+        'Hungary', 'Japan', 'Latvia', 'Liechtenstein', 'Poland', 'Slovakia',
+        'Slovenia', 'South Korea', 'Switzerland', 'Taiwan'
+    ]
+
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country in german_law)
+
+
+def Scandinavian_Law_Firm():
+    scandinavian_law = [
+        'Denmark',
+        'Finland',
+        'Iceland',
+        'Norway',
+        'Sweden'
+    ]
+
+    firms = list(db.matcher.match('Firm'))
+    for firm in firms:
+        country = firm['Incorporation_state']
+        print(country, country in scandinavian_law)
+
+
+def Transcontinental_case():
+    '''
+    Transcontinental_case (dummy, če je vsaj ena firma v Case-u izven Europe, potem 1, če je v Europe 0)
+    '''
+    for case in list(db.matcher.match('Case')):
+        firms_r = db.graph.run('MATCH (f:Firm)-[r]->(c:Case) where c.Case=$case RETURN f',
+                               case=case['Case']).data()
+
+        isTranscontinental = False
+        for firm_r in firms_r:
+            country = firm_r['f']['Incorporation_state']
+            continent = utils.getCountryInfo(country)['continent']
+            if continent != 'Europe':
+                isTranscontinental = True
                 break
 
-        if c_r is not None:
-            cinfo = utils.getCountryInfo(c_r.name)
-            if cinfo is not None:
-                print(cinfo.get('continent'), firm['Incorporation_state'])
-            else:
-                print('cinfo', firm['Incorporation_state'])
-        else:
-            print('c_r', firm['Incorporation_state'])
+        print(case['Case'], isTranscontinental)
+
+
+def Europe_only_case():
+    '''
+    Europe_only_case (dummy 01, obrnjeno od prenšnjega stolpca, če je v Case-u samo Europe 1, če je Extra_Europe potem 0)
+    '''
+    for case in list(db.matcher.match('Case')):
+        firms_r = db.graph.run('MATCH (f:Firm)-[r]->(c:Case) where c.Case=$case RETURN f',
+                               case=case['Case']).data()
+
+        isEuropeOnly = True
+        for firm_r in firms_r:
+            country = firm_r['f']['Incorporation_state']
+            continent = utils.getCountryInfo(country)['continent']
+            if continent != 'Europe':
+                isEuropeOnly = False
+                break
+
+        print(case['Case'], isEuropeOnly)
+
+
+def National_only_case():
+    '''National_only_case (dummy 01, če so vse Firme znotraj Case iz iste države, potem 1, drugače 0)'''
+    for case in list(db.matcher.match('Case')):
+        firms_r = db.graph.run('MATCH (f:Firm)-[r]->(c:Case) where c.Case=$case RETURN f',
+                               case=case['Case']).data()
+
+        theSameCountry = True
+        country = None
+        for firm_r in firms_r:
+            firm_country = firm_r['f']['Incorporation_state']
+            if country is None:
+                country = firm_country
+            elif country != firm_country:
+                theSameCountry = False
+                break
+
+        print(case['Case'], theSameCountry)
+
+def N_under_within_EC_case():
+    '''
+    Todo: Ali dam vsoto firm ki so znotraj katerega koli undertakinga???
+    N_firms_within_under (število firm notraj Undertaking-a znotraj Case-a)
+    '''
+
+    # for undertaking in list(db.matcher.match('Undertaking')):
+    #     num = db.graph.run('MATCH (f:Firm)-[r]->(u:Undertaking) where u.Undertaking=$undertaking RETURN count(r)',
+    #                        undertaking=undertaking['Undertaking']).data()[0]['count(r)']
+    #     print(num, '\t', undertaking['Undertaking'])
 
 if __name__ == '__main__':
     European_firm()
+
