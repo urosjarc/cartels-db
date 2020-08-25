5b642243b27487a4e3da7640d5a961137521a996
urosjarc <jar.fmf@gmail.com>
2020-08-25 16:43:36 +0200

    Country state of undertaking the same as firm cheking.

--- src/analysis.py
+++ src/analysis.py
@@ -109,7 +109,6 @@ def Repeat_firm_N_EC_cases():
         db.graph.push(firm)
 
 
-
 def Recidivist_firm_D():
     '''
         Recidivist_firm_D (dummy 01, če se pojavi samo enkrat v Case-ih, potem, če se pojavi vsaj 2-krat, potem 1)
@@ -121,6 +120,7 @@ def Recidivist_firm_D():
                            firm=firm['Firm']).data()[0]['count(r)']
         firm['Recidivist_firm_D'] = 1 if num >= 2 else 0
         db.graph.push(firm)
+
 
 def Recidivist_firm_2nd_time_D():
     '''
@@ -168,9 +168,10 @@ def Extra_Europe_firm():
         firm['Extra_Europe_firm'] = 1 if country in extra_eu else 0
         db.graph.push(firm)
 
+
 def EU_all_time_firm():
     slovarDrzav = {
-        'Ireland': '1/1/1973', # %m/%d/%Y
+        'Ireland': '1/1/1973',  # %m/%d/%Y
         'UK': '1/1/1973',
         'Belgium': '7/23/1952',
         'France': '7/23/1952',
@@ -204,10 +205,11 @@ def EU_all_time_firm():
         firm['EU_all_time_firm'] = 1 if datumPriklucitve is not None else 0
         db.graph.push(firm)
 
+
 def EU_EC_decision_firm():
     '''Todo:  Izspis'''
     slovarDrzav = {
-        'Ireland': '1/1/1973', # %m/%d/%Y
+        'Ireland': '1/1/1973',  # %m/%d/%Y
         'UK': '1/1/1973',
         'Belgium': '7/23/1952',
         'France': '7/23/1952',
@@ -281,6 +283,7 @@ def Japan_firm():
         firm['Japan_firm'] = 1 if country == 'Japan' else 0
         db.graph.push(firm)
 
+
 def Common_Law_Firm():
     countries_common_law = [
         'Australia', 'Bermuda', 'British Virgin Islands', 'Canada',
@@ -323,7 +326,7 @@ def French_Law_Firm():
         db.graph.push(firm)
 
 
-def German_Law():
+def German_Law_Firm():
     german_law = [
         'Austria', 'China', 'Croatia', 'Czech Republic', 'Estonia', 'Germany',
         'Hungary', 'Japan', 'Latvia', 'Liechtenstein', 'Poland', 'Slovakia',
@@ -345,7 +348,6 @@ def Scandinavian_Law_Firm():
         'Sweden'
     ]
 
-    firms = list()
     for firm in db.matcher.match('Firm'):
         country = firm['Incorporation_state']
         firm['Scandinavian_Law_Firm'] = 1 if country in scandinavian_law else 0
@@ -423,6 +425,7 @@ def N_under_within_EC_case():
     #                        undertaking=undertaking['Undertaking']).data()[0]['count(r)']
     #     print(num, '\t', undertaking['Undertaking'])
 
+
 def N_undertaking_within_EC_case():
     '''
     N_firms_within_EC_case (število vseh firm znotraj Case)
@@ -432,6 +435,7 @@ def N_undertaking_within_EC_case():
                            case=case['Case']).data()[0]['count(r)']
         case['N_undertaking_within_EC_case'] = int(num)
         db.graph.push(case)
+
 
 def Repeat_undertaking_N_EC_cases():
     '''
@@ -444,11 +448,6 @@ def Repeat_undertaking_N_EC_cases():
         undertaking['Repeat_undertaking_N_EC_cases'] = int(num)
         db.graph.push(undertaking)
 
-def Recidivist_undertaking_2nd_time_D():
-    '''
-    Todo: Recidivist_firm_2nd_time_D (dummy 01, ko se firma, ki je recidivist datumsko glede na EC_Date_of_decision prvič pojavi v bazi je tudi 0 (in ne 1 kot pri prejšnjem dummy-ju), ko se pa pojavi časovno gledano drugič, tretjič itd. pa je 1)
-    '''
-
 
 def Recidivist_undertaking_D():
     for undertaking in db.matcher.match('Undertaking'):
@@ -457,6 +456,232 @@ def Recidivist_undertaking_D():
         undertaking['Recidivist_undertaking_D'] = 1 if num >= 2 else 0
         db.graph.push(undertaking)
 
+
+def Recidivist_undertaking_2nd_time_D():
+    '''
+    Todo: Recidivist_firm_2nd_time_D (dummy 01, ko se firma, ki je recidivist datumsko glede na EC_Date_of_decision prvič pojavi v bazi je tudi 0 (in ne 1 kot pri prejšnjem dummy-ju), ko se pa pojavi časovno gledano drugič, tretjič itd. pa je 1)
+    Todo: Za izspis
+    '''
+
+
+def N_Undertaking_Inc_states_within_EC_case():
+    '''
+        N_Undertaking_Inc_states_within_EC_case (število vseh držav znotraj Case)
+    '''
+    for case in db.matcher.match('Case'):
+        undertakings_r = db.graph.run('MATCH (f:Undertaking)-[r]->(c:Case) where c.Case=$case RETURN f',
+                               case=case['Case']).data()
+
+        states = set()
+        for undertaking_r in undertakings_r:
+            states.add(undertaking_r['f']['Incorporation_state'])
+
+        case['N_Undertaking_Inc_states_within_EC_case'] = len(states)
+        db.graph.push(case)
+
+
+def European_undertaking():
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        cinfo = utils.getCountryInfo(country)
+        undertaking['European_undertaking'] = 1 if cinfo.get('continent') == 'Europe' else 0
+        db.graph.push(undertaking)
+
+
+def Extra_Europe_undertaking():
+    extra_eu = [
+        'Australia', 'Bermuda', 'British Virgin Islands', 'Canada',
+        'Cayman Islands', 'Hong Kong', 'India', 'Israel', 'Malaysia',
+        'New Zealand', 'Republic of South Africa', 'Singapore', 'USA', 'Brasil',
+        'Chile', 'Kuwait', 'Mexico', 'Tunisia', 'China', 'Japan', 'South Korea', 'Taiwan',
+    ]
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+
+        undertaking['Extra_Europe_undertaking'] = 1 if country in extra_eu else 0
+        db.graph.push(undertaking)
+
+
+def EU_all_time_undertaking():
+    slovarDrzav = {
+        'Ireland': '1/1/1973',  # %m/%d/%Y
+        'UK': '1/1/1973',
+        'Belgium': '7/23/1952',
+        'France': '7/23/1952',
+        'Greece': '1/1/1981',
+        'Italy': '7/23/1952',
+        'Lithuania': '5/1/2004',
+        'Luxembourg': '7/23/1952',
+        'Netherlands': '7/23/1952',
+        'Portugal': '1/1/1986',
+        'Romania': '1/1/2007',
+        'Spain': '1/1/1986',
+        'Austria': '1/1/1995',
+        'Croatia': '7/1/2013',
+        'Czech Republic': '5/1/2004',
+        'Estonia': '5/1/2004',
+        'Germany': '7/23/1952',
+        'Hungary': '5/1/2004',
+        'Latvia': '5/1/2004',
+        'Poland': '5/1/2004',
+        'Slovakia': '5/1/2004',
+        'Slovenia': '5/1/2004',
+        'Denmark': '1/1/1973',
+        'Finland': '1/1/1995',
+        'Sweden': '1/1/1995',
+    }
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        datumPriklucitve = slovarDrzav.get(country, None)
+        undertaking['EU_all_time_undertaking'] = 1 if datumPriklucitve is not None else 0
+        db.graph.push(undertaking)
+
+
+def EU_EC_decision_undertaking():
+    '''Todo:  Izspis'''
+    slovarDrzav = {
+        'Ireland': '1/1/1973',  # %m/%d/%Y
+        'UK': '1/1/1973',
+        'Belgium': '7/23/1952',
+        'France': '7/23/1952',
+        'Greece': '1/1/1981',
+        'Italy': '7/23/1952',
+        'Lithuania': '5/1/2004',
+        'Luxembourg': '7/23/1952',
+        'Netherlands': '7/23/1952',
+        'Portugal': '1/1/1986',
+        'Romania': '1/1/2007',
+        'Spain': '1/1/1986',
+        'Austria': '1/1/1995',
+        'Croatia': '7/1/2013',
+        'Czech Republic': '5/1/2004',
+        'Estonia': '5/1/2004',
+        'Germany': '7/23/1952',
+        'Hungary': '5/1/2004',
+        'Latvia': '5/1/2004',
+        'Poland': '5/1/2004',
+        'Slovakia': '5/1/2004',
+        'Slovenia': '5/1/2004',
+        'Denmark': '1/1/1973',
+        'Finland': '1/1/1995',
+        'Sweden': '1/1/1995',
+    }
+    '''
+    Todo: ????
+    EU_all_time_undertaking(dummy 01, če je bila kadarkoli v EU, 1, če ne 0) '''
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        cinfo = utils.getCountryInfo(country)
+        if cinfo.get('continent') == 'Europe':
+            datumPriklucitve = slovarDrzav.get(country, None)
+            if datumPriklucitve is not None:
+                cases_r = db.graph.run('MATCH (f:Undertaking)-[r]->(c:Case) where f.Undertaking=$undertaking RETURN c',
+                                       undertaking=undertaking['Undertaking']).data()
+
+                print(undertaking['Undertaking'])
+                for case in cases_r:
+                    case = case['c']
+                    EC_date = utils.parseDate(case['EC_Date_of_decision'])
+                    prikl_date = utils.parseDate(datumPriklucitve)
+
+                    if prikl_date < EC_date:
+                        print('\t', case['Case'], 1)
+                    else:
+                        print('\t', case['Case'], 0)
+                print('----------------------')
+
+def Old_EU_undertaking():
+    '''
+    Old_EU_undertaking(če gre za ustavno članico EU iz l. 1952, potem 1, drugače 0) '''
+    EU_founders = ['Belgium', 'France', 'Germany', 'Italy', 'Luxembourg', 'Netherland']
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['Old_EU_undertaking'] = 1 if country in EU_founders else 0
+        db.graph.push(undertaking)
+
+
+def USA_undertaking():
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['USA_undertaking'] = 1 if country == 'USA' else 0
+        db.graph.push(undertaking)
+
+
+def Japan_undertaking():
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['Japan_undertaking'] = 1 if country == 'Japan' else 0
+        db.graph.push(undertaking)
+
+
+def Common_Law_undertaking():
+    countries_common_law = [
+        'Australia', 'Bermuda', 'British Virgin Islands', 'Canada',
+        'Cayman Islands', 'Channel Islands', 'Hong Kong', 'India',
+        'Ireland', 'Israel', 'Malaysia', 'New Zealand', 'Republic of South Africa',
+        'Singapore', 'UK', 'USA']
+
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['Common_Law_Undertaking'] = 1 if country in countries_common_law else 0
+
+
+def English_Law_undertaking():
+    '''
+    English_Law_Undertaking (dummy 01, če je English 1, če ni 0)
+    '''
+
+    english_law = [
+        'Australia', 'Bermuda', 'British Virgin Islands', 'Canada', 'Cayman Islands', 'Channel Islands',
+        'Hong Kong', 'India', 'Ireland', 'Israel', 'Malaysia', 'New Zealand', 'Republic of South Africa', 'Singapore',
+        'UK', 'USA']
+
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['English_Law_Undertaking'] = 1 if country in english_law else 0
+        db.graph.push(undertaking)
+
+
+def French_Law_Undertaking():
+    french_law = [
+        'Belgium', 'Brasil', 'Chile', 'France',
+        'Greece', 'Italy', 'Kuwait', 'Lithuania',
+        'Luxembourg', 'Mexico', 'Netherlands', 'Portugal',
+        'Romania', 'Spain', 'Tunisia'
+    ]
+
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['French_Law_Undertaking'] = 1 if country in french_law else 0
+        db.graph.push(undertaking)
+
+
+def German_Law_Undertaking():
+    german_law = [
+        'Austria', 'China', 'Croatia', 'Czech Republic', 'Estonia', 'Germany',
+        'Hungary', 'Japan', 'Latvia', 'Liechtenstein', 'Poland', 'Slovakia',
+        'Slovenia', 'South Korea', 'Switzerland', 'Taiwan'
+    ]
+
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['German_Law'] = 1 if country in german_law else 0
+        db.graph.push(undertaking)
+
+
+def Scandinavian_Law_Undertaking():
+    scandinavian_law = [
+        'Denmark',
+        'Finland',
+        'Iceland',
+        'Norway',
+        'Sweden'
+    ]
+
+    for undertaking in db.matcher.match('Undertaking'):
+        country = undertaking['Incorporation_state']
+        undertaking['Scandinavian_Law_Undertaking'] = 1 if country in scandinavian_law else 0
+        db.graph.push(undertaking)
 
 
 def DUMMY_VARIABLES():
