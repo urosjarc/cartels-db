9027cd1e1e988a0b2f145542ece47d9b06bcde5b
urosjarc <jar.fmf@gmail.com>
2020-11-28 16:49:00 +0100

    Finish first part.

--- src/analysis_stock.py
+++ src/analysis_stock.py
@@ -70,6 +70,7 @@ def Stock_exchange_name():
             for sr in db.stock_meta_rows:
                 if row[ticker_type] == sr['Type']:
                     result = sr['BOURSE NAME']
+                    break
 
             if result is not None:
                 row[f'Stock_exchange_name_{type}'] = result
@@ -85,6 +86,176 @@ def Multiple_listings_D():
                     val = sr['STOCK EXCHANGE(S) LISTED']
                     if val != 'NA':
                         result = 1 if ',' in val else 0
+                        break
 
             if result is not None:
                 row[f'Multiple_listings_D_{type}'] = result
+
+def Stock_indexing_D():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Stock_indexing_D_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = 0 if sr['STOCK INDEX INFORMATION'] in ['NA', 'NOT USED'] else 1
+                    break
+
+            if result is not None:
+                row[f'Stock_indexing_D_{type}'] = result
+
+def Multishare_Corporation_D():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Multishare_Corporation_D_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = 1 if sr['IND-CURRENTLY MULT-SHARE CO'] == 'X'  else 0
+                    break
+
+            if result is not None:
+                row[f'Multishare_Corporation_D_{type}'] = result
+
+def ADR_D():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'ADR_D_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = 1 if sr['INDICATOR - ADR'] == 'X'  else 0
+                    break
+
+            if result is not None:
+                row[f'ADR_D_{type}'] = result
+
+def Board_Structure_Type():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Board_Structure_Type_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = None if sr['Board Structure Type'] == 'NA' else sr['Board Structure Type']
+                    break
+
+            if result is not None:
+                row[f'Board_Structure_Type_{type}'] = result
+
+def Product_market():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Product_market_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = None if sr['PRODUCTS'] == 'NA' else str(sr['PRODUCTS']).lower()
+                    break
+
+            if result is not None:
+                row[f'Product_market_{type}'] = result
+
+def Level_2_sector_name():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Level_2_sector_name_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['LEVEL 2 SECTOR']
+                    break
+
+            if result is not None:
+                row[f'Level_2_sector_name_{type}'] = result
+
+def Level_3_sector_name():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Level_3_sector_name_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['LEVEL 3 SECTOR NAME']
+                    break
+
+            if result is not None:
+                row[f'Level_3_sector_name_{type}'] = result
+
+def Level_4_sector_name():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Level_4_sector_name_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['LEVEL 4 SECTOR NAME']
+                    break
+
+            if result is not None:
+                row[f'Level_4_sector_name_{type}'] = result
+
+def Level_5_sector_name():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Level_5_sector_name_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['LEVEL 5 SECTOR NAME']
+                    break
+
+            if result is not None:
+                row[f'Level_5_sector_name_{type}'] = result
+
+def Local_index():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Local_index_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['LOCAL INDEX']
+                    break
+
+            if result is not None:
+                row[f'Local_index_{type}'] = result
+
+def Datastream_local_index():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'Datastream_local_index_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['DATASTREAM LOCAL INDEX']
+                    break
+
+            if result is not None:
+                row[f'Datastream_local_index_{type}'] = result
+
+def ICB_industry_DS_index():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'ICB_industry_DS_index_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['ICB Industry DS Index']
+                    break
+
+            if result is not None:
+                row[f'ICB_industry_DS_index_{type}'] = result
+
+def FT_sector_DS_index():
+    for type, ticker_type in mapping.items():
+        db.core_fields.append(f'FT_sector_DS_index_{type}')
+        for row in db.core:
+            result = None
+            for sr in db.stock_meta_rows:
+                if row[ticker_type] == sr['Type']:
+                    result = sr['ICB Industry DS Index']
+                    break
+
+            if result is not None:
+                row[f'FT_sector_DS_index_{type}'] = result
