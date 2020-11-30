9a6a4d87df03e96d22bfe4a24fd6113ab0e3220e
urosjarc <jar.fmf@gmail.com>
2020-11-30 18:36:59 +0100

    Small fixes on headings.

--- src/analysis_core_anual.py
+++ src/analysis_core_anual.py
@@ -1,6 +1,7 @@
 from src import db, utils
 
 def Ime_Currency_VAR(VAR, ticker='Ticker_firm', year_minus=0):
+    print("ANALYSIS CORE ANNUAL: ", VAR, year_minus)
     colums = set()
     for i, rows_group in enumerate([db.A1012M_EU_rows, db.A1012M_LOCAL_rows]):
         currency = "euros" if i==0 else "local"
@@ -8,6 +9,8 @@ def Ime_Currency_VAR(VAR, ticker='Ticker_firm', year_minus=0):
             for core_row in db.core:
                 if core_row[ticker] == annual_ticker:
                     for annual_row in annual_rows:
+                        if core_row[VAR] in [None, '']:
+                            continue
                         value = annual_row.get(str(int(core_row[VAR])-year_minus), None)
                         name = utils.getName(annual_row['Name'])
                         if name is None:
